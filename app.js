e388aec04ff93cf05ccd204d85115144a476254f
valeryshel <valeryshel@gmail.com>
2021-03-24 22:41:35 +0300

    refactor: add semantic tags according to rule 1.1(advanced) html-and-css.md

--- app.js
+++ app.js
@@ -58,8 +58,6 @@ var createNewTaskElement=function(taskString){
     return listItem;
 }
 
-
-
 var addTask=function(){
     console.log('Add Task...');
     //Create a new list item with the text from the #new-task:
@@ -69,9 +67,7 @@ var addTask=function(){
     //Append listItem to incompleteTaskHolder
     incompleteTaskHolder.appendChild(listItem);
     bindTaskEvents(listItem, taskCompleted);
-
     taskInput.value='';
-
 }
 
 //Edit an existing task.
@@ -79,10 +75,7 @@ var addTask=function(){
 var editTask=function(){
     console.log('Edit Task...');
     console.log('Change "edit" to "save"');
-
-
     var listItem=this.parentNode;
-
     var editInput=listItem.querySelector('input[type="text"]');
     var label=listItem.querySelector('label');
     var editBtn=listItem.querySelector('.edit-btn');
@@ -103,23 +96,19 @@ var editTask=function(){
     listItem.classList.toggle('edit-mode');
 };
 
-
 //Delete task.
 var deleteTask=function(){
     console.log('Delete Task...');
-
     var listItem=this.parentNode;
     var ul=listItem.parentNode;
     //Remove the parent list item from the ul.
     ul.removeChild(listItem);
-
 }
 
 
 //Mark task completed
 var taskCompleted=function(){
     console.log('Complete Task...');
-
     //Append the task list item to the #completed-tasks
     var listItem=this.parentNode;
     completedTasksHolder.appendChild(listItem);
